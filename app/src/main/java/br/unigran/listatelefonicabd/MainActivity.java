91ecb73da3ad63afd60098fe5100ffe38a536f7d
Luis <802.250@alunos.unigran.br>
2022-09-14 21:59:09 -0400

    commit para terminar em casa :D

--- app/src/main/java/br/unigran/listatelefonicabd/MainActivity.java
+++ app/src/main/java/br/unigran/listatelefonicabd/MainActivity.java
@@ -1,11 +1,17 @@
 package br.unigran.listatelefonicabd;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import bancoDeDados.ContatoDB;
@@ -23,6 +29,46 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //criação de conexão
+        db = new DBHelper(this);
 
+        //mapeamento dos campos da tela
+        nome = findViewById(idNome);
+        telefone = findViewById(idTelefone);
+        dataNasc = findViewById(idDataNasc);
+        lista = findViewById(idLista)
+
+        //alocação de lista
+        dados = new ArrayList();
+
+        //criação e vinculamento de adapter de lista
+        ArrayAdapter adapter = new ArrayAdapter(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, dados);
+        lista.setAdapter(adapter);
+
+        contatoDB = new ContatoDB(db);
+
+        //listagem inicial
+        contatoDB.listar(dados);
+
+        acoes();
+    }
+
+    private void acoes() {
+        lista.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                new AlertDialog.Builder(view.getContext())
+                        .setMessage("Deseja remover o contato?")
+                        .setPositiveButton("Confirmar", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                contatoDB.remover(dados.get(i).getId());
+                                contatoDB.listar(dados);
+                            }
+                        })
+                        .setNegativeButton("Cancelar", null)
+                        .create().show();
+            }
+        });
     }
 }
