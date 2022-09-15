5cf785cd9e8d8b2f0c48f66c567cb323922006ce
Luis <802.250@alunos.unigran.br>
2022-09-14 21:00:48 -0400

    criacao do projeto

---
+++ app/src/main/java/br/unigran/listatelefonicabd/MainActivity.java
@@ -0,0 +1,14 @@
+package br.unigran.listatelefonicabd;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
